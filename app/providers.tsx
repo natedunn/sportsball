2104467ac4453f00843c5368e583b110d9e76858
Nate Dunn <hello@natedunn.net>
2024-10-24 20:34:57 -0600

    fix: test revert changes

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,11 @@
 import { TrpcProvider } from "@/lib/trpc/TrpcProvider";
-// import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import { headers } from "next/headers";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-	return <TrpcProvider headers={headers()}>{children}</TrpcProvider>;
+	return (
+		<TrpcProvider headers={headers()}>
+			<ClerkProvider>{children}</ClerkProvider>
+		</TrpcProvider>
+	);
 }
