c92e252e9a96a09411dddda18fbfe8a65b48d0ab
Nate Dunn <hello@natedunn.net>
2024-10-24 20:24:00 -0600

    fix: remove clerk for nwo

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,7 @@
 import { TrpcProvider } from "@/lib/trpc/TrpcProvider";
-import { ClerkProvider } from "@clerk/nextjs";
+// import { ClerkProvider } from "@clerk/nextjs";
 import { headers } from "next/headers";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-	return (
-		<TrpcProvider headers={headers()}>
-			<ClerkProvider>{children}</ClerkProvider>
-		</TrpcProvider>
-	);
+	return <TrpcProvider headers={headers()}>{children}</TrpcProvider>;
 }
