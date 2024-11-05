d8793e8b4ad1b40fda9f38e447ac28370d8a9939
Nate Dunn <hello@natedunn.net>
2024-11-05 12:45:23 -0600

    feat: remove unused routes

--- app/api/scores/nba/team/[id]/route.ts
+++
@@ -1,9 +0,0 @@
-import { type NextRequest } from "next/server";
-
-export async function GET(
-	request: NextRequest,
-	{ params }: { params: Promise<{ id: string }> }
-) {
-	const id = (await params).id;
-	return new Response(`Hello from NBA API: ${id}`, request);
-}
