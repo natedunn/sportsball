d3cd9d76f7f25c10d6872104fd5b8a7d630c260f
Nate Dunn <hello@natedunn.net>
2024-11-19 22:16:06 -0600

    feat: style tweak

--- src/routes/nfl/+page.svelte
+++ src/routes/nfl/+page.svelte
@@ -1,4 +1,4 @@
 <div class="flex h-[calc(100vh-8rem)] w-full flex-col items-center justify-center gap-4">
 	<h1 class="text-6xl font-bold">NFL</h1>
-	<p>Coming soon</p>
+	<p class="text-muted-foreground">Coming soon</p>
 </div>
