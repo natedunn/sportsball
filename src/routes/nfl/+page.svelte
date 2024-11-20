ff03eddeffa89ee827c8959f10fe58967ac4a336
Nate Dunn <hello@natedunn.net>
2024-11-19 19:28:43 -0600

    chore: remove unused code

--- src/routes/nfl/+page.svelte
+++ src/routes/nfl/+page.svelte
@@ -1,6 +1,3 @@
-<script lang="ts">
-</script>
-
 <div class="flex h-[calc(100vh-8rem)] w-full flex-col items-center justify-center gap-4">
 	<h1 class="text-6xl font-bold">NFL</h1>
 	<p>Coming soon</p>
