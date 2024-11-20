b09a2cd5d0b236ca812d181508ec66cfe5ff9706
Nate Dunn <hello@natedunn.net>
2024-11-19 19:27:47 -0600

    feat: add nav

--- src/lib/components/mode-dropdown.svelte
+++ src/lib/components/mode-dropdown.svelte
@@ -1,19 +1,15 @@
 <script lang="ts">
-	import { buttonVariants } from '$lib/components/ui/button/index.js';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import { cn } from '$utils';
 	import Moon from 'lucide-svelte/icons/moon';
 	import Sun from 'lucide-svelte/icons/sun';
-	import { resetMode, setMode } from 'mode-watcher';
+	import { mode, resetMode, setMode } from 'mode-watcher';
 </script>
 
 <DropdownMenu.Root>
-	<DropdownMenu.Trigger class={buttonVariants({ variant: 'outline', size: 'icon' })}>
-		<Sun
-			class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-		/>
-		<Moon
-			class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-		/>
+	<DropdownMenu.Trigger>
+		<Sun size={16} class={cn($mode === 'dark' && 'hidden')} />
+		<Moon size={16} class={cn($mode !== 'dark' && 'hidden')} />
 		<span class="sr-only">Toggle theme</span>
 	</DropdownMenu.Trigger>
 	<DropdownMenu.Content align="end">
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,11 +19,23 @@
 </script>
 
 <QueryClientProvider client={queryClient}>
-	<div class="container">
-		<ModeWatcher />
-		<div>
-			<ModeDropdown />
-			{@render children()}
+	<ModeWatcher />
+
+	<nav class="border-b border-border bg-background">
+		<div class="container flex items-center justify-between">
+			<a class="group inline-flex justify-center gap-2 px-2 py-2" href="/">
+				<span class="py-0.5 text-lg leading-none">◍</span>
+				<span class="group-hocus:underline">Sportsball</span>
+			</a>
+			<div class="flex items-center gap-3">
+				<a class="inline-flex px-2 py-2 hocus:underline" href="/nba"> NBA </a>
+				<a class="inline-flex px-2 py-2 hocus:underline" href="/nfl"> NFL </a>
+				<div class="text-muted-foreground/50">|</div>
+				<ModeDropdown />
+			</div>
 		</div>
-	</div>
+	</nav>
+	<main class="container">
+		{@render children()}
+	</main>
 </QueryClientProvider>
