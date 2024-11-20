46c616a0cee6ca438cf0e2b684c52245c9612d96
Nate Dunn <hello@natedunn.net>
2024-11-19 19:28:12 -0600

    feat: placeholder

---
+++ src/routes/nfl/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<div class="flex h-[calc(100vh-8rem)] w-full flex-col items-center justify-center gap-4">
+	<h1 class="text-6xl font-bold">NFL</h1>
+	<p>Coming soon</p>
+</div>
